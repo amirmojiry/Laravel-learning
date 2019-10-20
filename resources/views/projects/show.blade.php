639416b556eec79f8e9eb138e3f07b1ee143081e
Amirhossein Mojiri Foroushani <amojiry@gmail.com>
2019-10-20 15:56:39 +0330

    add add task form

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -9,7 +9,7 @@
         <p><a href="/projects/{{ $project->id }}/eidt">Edit</a></p>
     </div>
     @if ($project->tasks->count())
-        <div>
+        <div class="box">
             @foreach ($project->tasks as $task)
                 <div>
                     <form action="/tasks/{{ $task->id }}" method="POST">
@@ -24,5 +24,20 @@
             @endforeach
         </div>
     @endif
+    <form method="POST" class="box">
+        <div class="field">
+            <label for="description" class="label">New Task</label>
+            <div class="control">
+                <input type="text" class="input">
+            </div>
+        </div>
+        <div class="field">
+            <div class="control">
+                <button type="submit" class="button is-link">
+                    Add Task
+                </button>
+            </div>
+        </div>
+    </form>
     <a href="/projects">Return</a>
 @endsection
